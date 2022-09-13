ae61aecca786eab8689bc3b19adb9b83cbfa7f2b
Averi <averi.sherman@gmail.com>
2022-09-13 08:39:04 -0400

    added questions to answer

--- index.js
+++ index.js
@@ -11,3 +11,42 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer
+ .prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Give a description of your project',
+        name: 'description',
+
+    },
+    {
+        type: 'input',
+        message: 'How would one install your project?',
+        name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: 'How would one use your project?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Give credit to anyone or anything thats due',
+        name: 'credit',
+    },
+    {
+        type: 'list',
+        massage: 'Which license will you use?',
+        choices: ["MIT", "Mozilla", "GNU General Public"],
+        name: 'license',
+    },
+
+ ])
