15429564be2ccaf395a8ebbeab36117b2e05831f
Aveheart <110785267+Aveheart@users.noreply.github.com>
2022-10-09 23:27:59 -0400

    deleted comments

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown");
-// TODO: Create an array of questions for user input
+
 const questions = [
     {
         type: 'input',
@@ -55,8 +54,6 @@ const questions = [
  ];
 
 
-
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
 
@@ -64,7 +61,6 @@ function writeToFile(fileName, data) {
     )
 }
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt (questions)
     .then((answers) => {
@@ -73,5 +69,4 @@ function init() {
     })
 }
 
-// Function call to initialize app
 init();
